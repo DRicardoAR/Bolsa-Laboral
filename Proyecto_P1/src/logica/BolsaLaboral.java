8ef029313980c30177243d8c5342157dcf071c92
ariel <ariel@Telematico55>
2017-07-03 12:07:07 -0400

    Add dos atributos en clase principal.

--- Proyecto_P1/src/logica/BolsaLaboral.java
+++ Proyecto_P1/src/logica/BolsaLaboral.java
@@ -1,5 +1,14 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class BolsaLaboral {
+	private ArrayList<Empresa> misEmpresas;
+	private ArrayList<Persona> misPersonas;
+	
+	
+	
+	
+	
 
 }
