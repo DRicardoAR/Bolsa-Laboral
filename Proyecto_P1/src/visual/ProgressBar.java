b2c325f9f82a11ab1e1d84ea55bdb0e0e72a8241
arielfelixiii <arielfelixiii@gmail.com>
2017-07-22 15:31:20 -0400

    update

--- Proyecto_P1/src/visual/ProgressBar.java
+++ Proyecto_P1/src/visual/ProgressBar.java
@@ -18,6 +18,7 @@ public class ProgressBar extends JFrame {
 	 * Create the frame.
 	 */
 	public ProgressBar() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
